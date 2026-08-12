9f5938929faf3da97c4ebc5a55bf3de8eb252e67
hmdshahdat501-png <hmdshahdat501@gmail.comm>
2026-08-12 08:24:41 +0600

    ser set

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-import app from './app.js';
+// import app from './app.js';
 
-const PORT = Number(process.env.PORT) || 5000;
+// const PORT = Number(process.env.PORT) || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
